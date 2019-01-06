f559fd4e36c9635b41a893297b4682589b1133b2
Archaeologist03 <omniiknight@hotmail.com>
2019-01-06 19:40:08 +0100

    Tweaking about, trying to fix mobile 100vh issue

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -8,9 +8,9 @@ import Particles from './ParticlesBG/ParticlesBG'
 function Background(props) {
   return (
     <div className="background-container">
-      <img className="background-container__image" src={props.img} alt="background" />
+      <img className="background-container__image" src={props.img} alt="background" height="100%" />
       <div className="background-container__overlay"></div>
-      <Particles />
+      {/* <Particles /> */}
       {/* <SphereGif /> */}
     </div>
   );
