ae4ed09047936fcbab3ef5e3efd0ce29fb2b3233
Archaeologist03 <omniiknight@hotmail.com>
2020-02-23 01:45:10 +0100

    Interests text update, new bottom-border for projects, cleared some commeted code.

--- src/containers/About/Interests/Interests.js
+++ src/containers/About/Interests/Interests.js
@@ -5,7 +5,9 @@ import './Interests.scss';
 function Interests() {
 	return (
 		<div className='interests-container'>
-			<span className='title'>Hobbies and Interests:</span>
+			<span className='title'>
+				Hobbies, Interests, and other Personal stuff:{' '}
+			</span>
 			<div>
 				<span className='dash'>-</span> Reading
 				<br />
@@ -16,6 +18,34 @@ function Interests() {
 				interest?
 			</div>
 			<span className='dash'>-</span> I like to write now and then.
+			<br />
+			<br />
+			Pih, this looks like a grocery store list.. I don't like it. I'll try more
+			personal write up.
+			<br />
+			At this point of life, it is struggle to make balance between "hobbies"
+			and "work", I am sure you, dear reader, felt the same at one point.
+			<br />
+			First two things from "grocery list" are a must for me. I can live without
+			third thing though.
+			<br />
+			If we're playing basketball I'm taking number 3 because of Allen Iverson.
+			<br />
+			I am also letting my halfway beard grow because of Dostoyevsky (ok, not,
+			maybe, partly, but, yeah..)
+			<br />
+			Beside those I should mention that I appreciate cinema, Mr. Robot is my
+			absolutely favorite piece of it. 12 Angry Man is my favorite classic.
+			<br />
+			In recent years I started to appreciate so many music genres, lately I
+			found Future Funk, it is.. funky.. While I code I am most productive
+			having Synthwave running, but sometimes I feel more Hip-Hopish, my Hip Hop
+			knowledge is as good as my JS knowledge(hehe) (my favorite is Nas), I'm
+			also appreciating 70s and 80s music, rock, pop, dance, jazz.. I also love
+			Pink Floyd.
+			<br />
+			Okay, that's about way too much, I could probably hide my worst stuff here
+			and nobody would ever know about it.
 			<br />
 		</div>
 	);
